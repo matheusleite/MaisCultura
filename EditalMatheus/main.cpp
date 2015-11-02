2798e698b9b3c87fc079c65867d8d876762b8f70
Matheus Clemente <matheus_klementt@hotmail.com>
2015-11-02 14:22:27 -0200

    Correção funções adicionar e exibir

--- EditalMatheus/main.cpp
+++ EditalMatheus/main.cpp
@@ -1,29 +1,76 @@
-#include <iostream>
+#include <stdlib.h>
 #include <stdio.h>
-using namespace std;
 
-void read (){
+void read ()
+{
     FILE *f;
     f = fopen("edital.txt","r");
-    char edital[150];
-    while (fgets(edital, 100, f)!= NULL){
+    char edital[1000];
+    while (fgets(edital, 1000, f)!= NULL)
+    {
         printf("%s", edital);
     }
     printf ("\n");
     fclose(f);
 }
-
-int main()
+void edit()
 {
     FILE *f;
     f = fopen("edital.txt","a");
-    read();
     char edit[150];
     printf("Digite o edital a ser adicionado: ");
-    gets(edit);
+    fgets(edit,150,stdin);
     fputs(edit, f);
-    fprintf(f,"\n");
     fclose(f);
-    read();
+}
+void remover()
+{
+    /*FILE *f1, *f2;
+    int linha=1, linha_deletar;
+    char edital[1000];
+    f1 = fopen("edital.txt","r");
+    f2 = fopen("edital2.txt","w");
+    printf("Digite a linha a ser removida: ");
+    scanf("%d", &linha_deletar);
+    getchar();
+    while (fgets(edital, 1000, f1)!= NULL)
+    {
+        if (=='\n'){
+            linha++;
+        }
+        if(linha != linha_deletar)
+            fputs(edital, f2);
+    }*/
+
+}
+int main()
+{
+    int n;
+    printf("MENU\n");
+    printf("1. Adicionar edital\n");
+    printf("2. Listar editais\n");
+    printf("3. Editar edital\n");
+    printf("4. Remover edital\n");
+    printf("0. Voltar\n");
+    scanf("%d", &n);
+    getchar();
+    switch (n)
+    {
+    case 1:
+        edit();
+        break;
+    case 2:
+        read();
+        break;
+    case 3:
+        read();
+        edit();
+        break;
+    case 4:
+        printf("\n");
+        read();
+        remover();
+        break;
+    }
     return 0;
 }
