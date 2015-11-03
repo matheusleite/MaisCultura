3d37dff788b759334c74a42e0a0b97f01a14fcf1
leticiadesouza <leticia.souza212@gmail.com>
2015-11-02 23:56:28 -0200

    Update main.cpp

--- ProjetoPDS/main.cpp
+++ ProjetoPDS/main.cpp
@@ -1,32 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void create(char *concerto, char *local, char *data){
-}
+int main(){
+    
 
-void list(){
-}
+int ans = 1;
+	do{
 
-void edit(){
-}
-
-void remove(){
-}
-
-int main()
-{
     printf("MENU\n");
     printf("1. Adicionar concerto\n");
     printf("2. Listar concertos\n");
     printf("3. Editar concerto\n");
     printf("4. Remover concerto\n");
     printf("0. Voltar\n");
-    int ans;
     scanf("%d", &ans);
-
+    FILE *dados;
+    
     switch(ans){
 
         case 1:
+            
+            
+        	dados = fopen("dados.txt", "a");
+        	
             char nome_concerto[100], local[100], data_evento[30];
             printf("Adicionar concerto");
             printf("\nInsira o nome do concerto: ");
@@ -36,22 +32,26 @@ int main()
             printf("\nInsira a data do evento: ");
             scanf("%s", &data_evento);
 
-            create(nome_concerto, local, data_evento);
+            fprintf(dados, "%s, %s, %s\n", nome_concerto, local, data_evento);
+            fclose(dados);
+            printf("Concerto adicionado ao arquivo");
+            
             break;
 
         case 2:
-            list();
+
             break;
 
         case 3:
-            edit();
+
             break;
 
         case 4:
-            remove();
+
             break;
-
-
     }
+
+    }while(ans!=0);
+
     return 0;
 }
