8a33c78f753b3793fa7d6c2a1d6e6fe424e73ab8
ateldyfilho <ateldybfilho@gmail.com>
2015-11-03 13:05:51 -0200

    Update and rename main.cpp to main.c

--- ProjetoPDS/main.c
+++ ProjetoPDS/main.c
@@ -1,8 +1,9 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(){
-    
+
 
 int ans = 1;
 	do{
@@ -15,14 +16,14 @@ int ans = 1;
     printf("0. Voltar\n");
     scanf("%d", &ans);
     FILE *dados;
-    
+
     switch(ans){
 
         case 1:
-            
-            
+
+
         	dados = fopen("dados.txt", "a");
-        	
+
             char nome_concerto[100], local[100], data_evento[30];
             printf("Adicionar concerto");
             printf("\nInsira o nome do concerto: ");
@@ -34,12 +35,42 @@ int ans = 1;
 
             fprintf(dados, "%s, %s, %s\n", nome_concerto, local, data_evento);
             fclose(dados);
-            printf("Concerto adicionado ao arquivo");
-            
+            printf("Concerto adicionado ao arquivo.\n");
+
             break;
 
         case 2:
 
+            printf("\nVisualizar Concertos: \n\n");
+
+            dados = fopen("dados.txt", "r");
+
+
+            int nLinhas=0;
+
+            char c;
+            while(!feof(dados)){
+                c = fgetc(dados);
+                if(c =='\n'){
+                    nLinhas++;
+                }
+            }
+            rewind(dados);
+
+
+            char line[150];
+            int n = 1;
+            int count=0;
+
+            while(count<nLinhas){
+                fgets(line, 150, dados);
+                printf("%d - %s", n, line);
+                n++;
+                count++;
+            }
+
+            fclose(dados);
+            printf("\n");
             break;
 
         case 3:
